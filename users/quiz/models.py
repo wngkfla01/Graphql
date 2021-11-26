68b4b506c8bad85d339900dc9a38d27d22d27c44
wngkfla01 <wngkfla01@gmail.com>
2021-11-26 16:15:25 +0900

    first commit

---
+++ users/quiz/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Quizzes(models.Model):
+    title = models.CharField(max_length=255, default=_("New Quiz"))
+    category = models.ForeignKey(Category, default=1, on_delete=models.DO_NOTHING)  # DO_NOTING: '아무런 행동을 취하지 않는다.', 참조무결성을 해칠 위험이 있어서 잘 사용되지는 않는다.
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+
+    SCALE = (
+        (0, _('Fundamental')),
+        (1, _('Beginner')),
+        (2, _('Intermediate')),
+        (3, _('Advanced')),
+        (4, _('Expert'))
+    )
+
+    TYPE = (
+        (0, _('Multiple Choice')),
+    )
+
+    quiz = models.ForeignKey(Quizzes, related_name='question', on_delete=models.DO_NOTHING)
+    technique = models.IntegerField(choices=TYPE, default=0, verbose_name=_("Type of Question"))
+    title = models.CharField(max_length=255, verbose_name=_("Title"))
+    difficulty = models.IntegerField(choices=SCALE, default=0, verbose_name=_("Difficulty"))
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name=_("Date Created"))
+    is_active = models.BooleanField(default=False, verbose_name=_("Active Status"))
+
+    def __str__(self):
+        return self.title
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, related_name='answer', on_delete=models.DO_NOTHING)
+    answer_text = models.CharField(max_length=255, verbose_name=_("Answer Text"))
+    is_right = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.answer_text
---
+++ users/quiz/schema.py
@@ -0,0 +1,69 @@
+import graphene
+from graphene_django import DjangoObjectType
+from graphene_django import DjangoListField
+from .models import Quizzes, Category, Question, Answer
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ("id","name")
+
+class QuizzesType(DjangoObjectType):
+    class Meta:
+        model = Quizzes
+        fields = ("id", "title", "category", "quiz")
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = ("title", "quiz")
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = ("question", "answer_text")
+
+class Query(graphene.ObjectType):
+    all_questions = graphene.Field(QuestionType, id=graphene.Int())
+    all_answers = graphene.List(AnswerType, id=graphene.Int())
+
+    def resolve_all_questions(root, info, id):
+        return Question.objects.get(pk=id)
+    def resolve_all_answers(root, info, id):
+        return Answer.objects.filter(question=id)
+
+# class CategoryMutation(graphene.Mutation):
+#     class Arguments:
+#         name = graphene.String(required=True)
+
+#     category = graphene.Field(CategoryType)
+
+#     @classmethod
+#     def mutate(cls, root, info, name):
+#         category = Category(name=name)
+#         category.save()
+#         return CategoryMutation(category=category)
+
+# create, update와 delete는 비슷비슷하다!
+class CategoryMutation(graphene.Mutation):
+    class Arguments: 
+        id = graphene.ID()
+
+    category = graphene.Field(CategoryType)
+
+    @classmethod
+    def mutate(cls, root, info, id):
+        category = Category.objects.get(id=id)
+        category.delete()
+        return 
+
+class Mutation(graphene.ObjectType):
+
+    update_category = CategoryMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
+
+
+    
